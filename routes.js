307cf38c10c5388d083d5da6ad18ea7c115794d0
Jose Martinez <martinezjoja@gmail.com>
2019-10-21 01:21:08 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,18 +1,20 @@
 //Declare
 const path = require('path')
 const glob = require('glob')
-const app = require( path.join( process.cwd() ) )
-//configuration
-let controllers = { }
-let files = glob.sync( path.join( process.cwd(), 'controllers', '**', '*.js' ) );
-files.forEach( function( file ) {
+//
+const app = require(path.join(process.cwd()))
+
+// configuration
+const controllers = { }
+const files = glob.sync(path.join(process.cwd(), 'controllers', '**', '*.js'));
+files.forEach((file) => {
   let temp = controllers
-  let parts = path.relative( path.join( process.cwd(), 'controllers' ), file ).slice( 0, -3 ).split( path.sep )
-  while( parts.length ){
-    if( parts.length === 1)   temp[parts[0]] = require( file )
-    else temp[ parts[0] ] = temp[parts[0]] || {}
+  const parts = path.relative(path.join(process.cwd(), 'controllers'), file).slice(0, -3).split(path.sep)
+  while (parts.length) {
+    if (parts.length === 1) temp[parts[0]] = require(file)
+    else temp[parts[0]] = temp[parts[0]] || {}
 
     temp = temp[parts.shift()];
   }
-} )
-module.exports = function() { require('./routes/index')( app, controllers ) }
+})
+module.exports = function() { require('./routes/index')(app, controllers) }
