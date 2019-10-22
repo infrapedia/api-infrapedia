cfbe980969842d3fd42b9d2bc93356938b46a8b6
Jose Martinez <martinezjoja@gmail.com>
2019-10-21 21:30:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-underscore-dangle
 // Declare
 require('dotenv/config')
-const _path = require('path');
-const _express = require('express')
+const path = require('path');
+const express = require('express')
 const db = require('./config/connection');
 // Express config
-const app = _express()
+const app = express()
 require('./config/setConfigExpress')(app)
+
 module.exports = app
 // routes
-require(_path.join(process.cwd(), 'routes'))();
+require(path.join(process.cwd(), 'routes'))()
+
 // Run Server
 // connect database
 db.connect(app.get('settings').mongodb.domain, app.get('settings').mongodb.name, (err) => { if (err) throw Error(err); });
