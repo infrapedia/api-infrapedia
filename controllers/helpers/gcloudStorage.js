4056b24fe7d78ff8bb384095e95c3082a03209c5
Jose Martinez <martinezjoja@gmail.com>
2020-01-08 16:56:52 -0500

    Local

--- controllers/helpers/gcloudStorage.js
+++ controllers/helpers/gcloudStorage.js
@@ -11,53 +11,54 @@ const bucket = storage.bucket(process.env._GG_CLOUD_BUCKET);
 const allowedExtensionsImg = /(\.jpg|\.jpeg)$/i;
 const allowedExtensionsKmz = /(\.kmz|\.KMZ)$/i;
 
-// function uploadFileLogo(path, user, allowedExtensions) {
-//   return new Promise((resolve, reject) => {
-//     if (allowedExtensions.exec(path)) {
-//       const ufile = `logo-${uuidv4()}${allowedExtensionsImg.exec(path)[1]}`;
-//       const bucketFile = bucket.file(`${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${user}/${ufile}`);
-//       fs.createReadStream(path)
-//         .pipe(bucketFile.createWriteStream({
-//           metadata: {
-//             contentType: mime.getType(path),
-//           },
-//         })).on('error', (err) => { reject(err); }).on('finish', () => {
-//           bucketFile.makePublic().then(() => {
-//             // resolve(`https://clients.agrimanager.app/${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${ufile}`);
-//             resolve(`https://storage.googleapis.com/${process.env._GG_CLOUD_BUCKET}/${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${user}/${ufile}`);
-//           });
-//         });
-//     } else {
-//       resolve(0);
-//     }
-//   });
-// }
-//
-// function uploadFileKmz(path, user, allowedExtensions) {
-//   return new Promise((resolve, reject) => {
-//     if (allowedExtensions.exec(path)) {
-//       const ufile = `kmz-${uuidv4()}${allowedExtensions.exec(path)[1]}`;
-//       const bucketFile = bucket.file(`${process.env._GG_CLOUD_BUCKET_FOLDER_KMZ}/${user}/${ufile}`);
-//       fs.createReadStream(path)
-//         .pipe(bucketFile.createWriteStream({
-//           metadata: {
-//             contentType: mime.getType(path),
-//           },
-//         })).on('error', (err) => { reject(err); }).on('finish', () => {
-//           bucketFile.makePublic().then(() => {
-//           // resolve(`https://clients.agrimanager.app/${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${ufile}`);
-//             resolve(`https://storage.googleapis.com/${process.env._GG_CLOUD_BUCKET}/${process.env._GG_CLOUD_BUCKET_FOLDER_KMZ}/${user}/${ufile}`);
-//           });
-//         });
-//     } else {
-//       resolve(0);
-//     }
-//   });
-// }
+function uploadFileLogo(path, user, allowedExtensions, folder) {
+  return new Promise((resolve, reject) => {
+    if (allowedExtensions.exec(path)) {
+      const ufile = `logo-${uuidv4()}${allowedExtensionsImg.exec(path)[1]}`;
+      const bucketFile = bucket.file(`${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${user}/${ufile}`);
+      fs.createReadStream(path)
+        .pipe(bucketFile.createWriteStream({
+          metadata: {
+            contentType: mime.getType(path),
+          },
+        })).on('error', (err) => { reject(err); }).on('finish', () => {
+          bucketFile.makePublic().then(() => {
+            // resolve(`https://clients.agrimanager.app/${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${ufile}`);
+            resolve(`https://storage.googleapis.com/${process.env._GG_CLOUD_BUCKET}/${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${user}/${ufile}`);
+          });
+        });
+    } else {
+      resolve(0);
+    }
+  });
+}
+
+function uploadFileKmz(path, user, allowedExtensions, folder) {
+  return new Promise((resolve, reject) => {
+    if (allowedExtensions.exec(path)) {
+      const ufile = `kmz-${uuidv4()}${allowedExtensions.exec(path)[1]}`;
+      const bucketFile = bucket.file(`${process.env._GG_CLOUD_BUCKET_FOLDER_KMZ}/${user}/${ufile}`);
+      fs.createReadStream(path)
+        .pipe(bucketFile.createWriteStream({
+          metadata: {
+            contentType: mime.getType(path),
+          },
+        })).on('error', (err) => { reject(err); }).on('finish', () => {
+          bucketFile.makePublic().then(() => {
+          // resolve(`https://clients.agrimanager.app/${process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS}/${ufile}`);
+            resolve(`https://storage.googleapis.com/${process.env._GG_CLOUD_BUCKET}/${process.env._GG_CLOUD_BUCKET_FOLDER_KMZ}/${user}/${ufile}`);
+          });
+        });
+    } else {
+      resolve(0);
+    }
+  });
+}
 
 function upload(path, user, allowedExtensions, folder) {
   return new Promise((resolve, reject) => {
     if (allowedExtensions.exec(path)) {
+      console.log('testing')
       const ufile = `${folder}-${uuidv4()}${allowedExtensions.exec(path)[1]}`;
       const bucketFile = bucket.file(`${folder}/${user}/${ufile}`);
       fs.createReadStream(path)
@@ -84,11 +85,11 @@ module.exports = {
       if (files === undefined) {
         resolve([]);
       } else if (Array.isArray(files)) {
-        Promise.all(files.map((x) => upload(x.path, user, allowedExtensionsImg, process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS))).then((filesArray) => {
+        Promise.all(files.map((x) => uploadFileLogo(x.path, user, allowedExtensionsImg, process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS))).then((filesArray) => {
           resolve(filesArray.filter(Boolean));
         })
           .catch(() => { resolve([]); });
-      } else { upload(files.path, user, allowedExtensionsImg, process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS).then((fileArray) => { resolve((fileArray !== 0) ? [fileArray] : []); }).catch(() => { resolve([]); }); }
+      } else { uploadFileLogo(files.path, user, allowedExtensionsImg, process.env._GG_CLOUD_BUCKET_FOLDER_LOGOS).then((fileArray) => { resolve((fileArray !== 0) ? [fileArray] : []); }).catch(() => { resolve([]); }); }
     } catch (err) { resolve(err); }
   }),
   kmzUploadFiles: (files, user) => new Promise((resolve) => {
@@ -96,10 +97,10 @@ module.exports = {
       if (files === undefined) {
         resolve([]);
       } else if (Array.isArray(files)) {
-        Promise.all(files.map((x) => upload(x.path, user, allowedExtensionsKmz, process.env._GG_CLOUD_BUCKET_FOLDER_KMZ))).then((filesArray) => {
+        Promise.all(files.map((x) => uploadFileKmz(x.path, user, allowedExtensionsKmz, process.env._GG_CLOUD_BUCKET_FOLDER_KMZ))).then((filesArray) => {
           resolve(filesArray.filter(Boolean));
         }).catch(() => { resolve([]); });
-      } else { upload(files.path, user, allowedExtensionsKmz, process.env._GG_CLOUD_BUCKET_FOLDER_KMZ).then((fileArray) => { resolve((fileArray !== 0) ? [fileArray] : []); }).catch(() => { resolve([]); }); }
+      } else { uploadFileKmz(files.path, user, allowedExtensionsKmz, process.env._GG_CLOUD_BUCKET_FOLDER_KMZ).then((fileArray) => { resolve((fileArray !== 0) ? [fileArray] : []); }).catch(() => { resolve([]); }); }
     } catch (err) { resolve(err); }
   }),
 };
