d4838c31fca27590f37ebbb8cabe80a1b690e4db
Jose Martinez <martinezjoja@gmail.com>
2020-01-21 07:46:36 -0500

    Update CableLandingStation.js

--- controllers/class/CableLandingStation.js
+++ controllers/class/CableLandingStation.js
@@ -297,6 +297,21 @@ class CLS {
               },
             },
             {
+              $lookup: {
+                from: 'alerts',
+                let: { elemnt: { $toString: '$_id' } },
+                pipeline: [
+                  {
+                    $match: { $and: [{ $expr: { elemnt: '$$elemnt' } }, { t: 'cls' }, { uuid: user }, { disabled: false }] },
+                  },
+                ],
+                as: 'alert',
+              },
+            },
+            {
+              $addFields: { alert: { $size: '$alert' } },
+            },
+            {
               $project: {
                 _id: 0,
                 uuid: 0,
