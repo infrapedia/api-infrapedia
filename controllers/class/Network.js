a343ad2ef4d906cf535f6d45b4361640dafc8cd7
Jose Martinez <martinezjoja@gmail.com>
2020-01-22 18:09:16 -0500

    Update Network.js

--- controllers/class/Network.js
+++ controllers/class/Network.js
@@ -254,7 +254,7 @@ class Network {
                 let: { elemnt: { $toString: '$_id' } },
                 pipeline: [
                   {
-                    $match: { $and: [{ $expr: { elemnt: '$$elemnt' } }, { t: 'networkc' }, { uuid: user }, { disabled: false }] },
+                    $match: { $and: [{ $expr: { elemnt: '$$elemnt' } }, { t: 'network' }, { uuid: user }, { disabled: false }] },
                   },
                 ],
                 as: 'alert',
