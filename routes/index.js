3ec954b7aecb0d291f5fc400dfdcec824db51f08
Jose Martinez <martinezjoja@gmail.com>
2020-02-04 12:53:18 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,6 @@ const routes = function (router, controllers) {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
-  
   // Response information
 
   const response = {
