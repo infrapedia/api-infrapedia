c3110966288f0eee80472a5e92534363c3db7840
Jose Martinez <martinezjoja@gmail.com>
2020-02-21 23:54:04 -0500

    Update cable.model.js

--- models/cable.model.js
+++ models/cable.model.js
@@ -3,35 +3,36 @@ const db = require('../config/connection');
 module.exports = function () {
   return new Promise((resolve, reject) => {
     db.get().createCollection('cables',
-      {
-        validator: {
-          $jsonSchema: {
-            bsonType: 'object',
-            required: ['uuid'],
-            properties: {
-              uuid: { bsonType: 'string' },
-              name: { bsonType: 'string' },
-              system_lenght: { bsonType: 'double' },
-              activationDateTime: { bsonType: 'date' }, // Convert to timestamp
-              urls: { bsonType: 'array' },
-              terrestrial: { bsonType: 'bool' },
-              capacity_tbps: { bsonType: 'double' },
-              fiberPairs: { bsonType: 'string' },
-              notes: { bsonType: 'string' },
-              category: { bsonType: 'string' },
-              facilities: { bsonType: 'array' }, // Array of object ID's
-              // cls: { bsonType: 'array' }, // Array of object ID's
-              geometry: { bsonType: 'object' },
-              rgDate: { bsonType: 'date' },
-              uDate: { bsonType: 'date' },
-              status: { bsonType: 'bool' },
-              deleted: { bsonType: 'bool' },
-            },
-          },
-          validationLevel: 'off',
-          validationAction: 'warn',
-        },
-      }).then((network) => { resolve(network); }).catch((err) => {
+      // {
+      //   validator: {
+      //     $jsonSchema: {
+      //       bsonType: 'object',
+      //       required: ['uuid'],
+      //       properties: {
+      //         uuid: { bsonType: 'string' },
+      //         name: { bsonType: 'string' },
+      //         system_lenght: { bsonType: 'double' },
+      //         activationDateTime: { bsonType: 'date' }, // Convert to timestamp
+      //         urls: { bsonType: 'array' },
+      //         terrestrial: { bsonType: 'bool' },
+      //         capacity_tbps: { bsonType: 'double' },
+      //         fiberPairs: { bsonType: 'string' },
+      //         notes: { bsonType: 'string' },
+      //         category: { bsonType: 'string' },
+      //         facilities: { bsonType: 'array' }, // Array of object ID's
+      //         // cls: { bsonType: 'array' }, // Array of object ID's
+      //         geometry: { bsonType: 'object' },
+      //         rgDate: { bsonType: 'date' },
+      //         uDate: { bsonType: 'date' },
+      //         status: { bsonType: 'bool' },
+      //         deleted: { bsonType: 'bool' },
+      //       },
+      //     },
+      //     validationLevel: 'off',
+      //     validationAction: 'warn',
+      //   },
+      // }
+      ).then((network) => { resolve(network); }).catch((err) => {
       reject(err);
     });
   });
