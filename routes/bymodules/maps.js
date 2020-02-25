253bab8a79c46318c910e6d26d1cfd9d8deff903
Jose Martinez <martinezjoja@gmail.com>
2020-02-25 12:23:30 -0500

    Update maps.js

--- routes/bymodules/maps.js
+++ routes/bymodules/maps.js
@@ -2,12 +2,12 @@ module.exports = {
   callEndPoints: (router, controllers, response) => {
     const statics = require('../../lib/middleware/statics');
     router.post(`${process.env._ROUTE}/auth/map/mymap`, (req, res) => {
-      controllers.maps.myMap(req.headers.user_id, req.body)
+      controllers.maps.myMap(req.headers.userid, req.body)
         .then((r) => { response.success(res, r); })
         .catch((e) => { response.err(res, e); });
     });
     router.get(`${process.env._ROUTE}/auth/map/mymap`, (req, res) => {
-      controllers.maps.getMyMap(req.headers.user_id)
+      controllers.maps.getMyMap(req.headers.userid)
         .then((r) => { response.success(res, r, false); })
         .catch((e) => { response.err(res, e); });
     });
