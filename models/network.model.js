1f9241db63de4304393caadd2917fa566a199aec
Jose Martinez <martinezjoja@gmail.com>
2020-05-09 20:36:55 -0500

    Remove validator

--- models/network.model.js
+++ models/network.model.js
@@ -3,30 +3,31 @@ const db = require('../config/connection');
 module.exports = function () {
   return new Promise((resolve, reject) => {
     db.get().createCollection('networks',
-      {
-        validator: {
-          $jsonSchema: {
-            bsonType: 'object',
-            required: ['uuid'],
-            properties: {
-              uuid: { bsonType: 'string' },
-              name: { bsonType: 'string' },
-              websites: { bsonType: ['array'] }, // Array of strings
-              organizations: { bsonType: ['array'] },
-              facilities: { bsonType: ['array'] }, // Array of object IDs
-              ixps: { bsonType: ['array'] }, // Array of object ID's
-              cls: { bsonType: ['array'] }, // Array of object ID's
-              cables: { bsonType: ['array'] }, // Array of object ID's
-              rgDate: { bsonType: 'date' },
-              uDate: { bsonType: 'date' },
-              status: { bsonType: 'bool' },
-              deleted: { bsonType: 'bool' },
-            },
-          },
-          validationLevel: 'off',
-          validationAction: 'warn',
-        },
-      }).then((network) => { resolve(network); }).catch((err) => {
+      // {
+      //   validator: {
+      //     $jsonSchema: {
+      //       bsonType: 'object',
+      //       required: ['uuid'],
+      //       properties: {
+      //         uuid: { bsonType: 'string' },
+      //         name: { bsonType: 'string' },
+      //         websites: { bsonType: ['array'] }, // Array of strings
+      //         organizations: { bsonType: ['array'] },
+      //         facilities: { bsonType: ['array'] }, // Array of object IDs
+      //         ixps: { bsonType: ['array'] }, // Array of object ID's
+      //         cls: { bsonType: ['array'] }, // Array of object ID's
+      //         cables: { bsonType: ['array'] }, // Array of object ID's
+      //         rgDate: { bsonType: 'date' },
+      //         uDate: { bsonType: 'date' },
+      //         status: { bsonType: 'bool' },
+      //         deleted: { bsonType: 'bool' },
+      //       },
+      //     },
+      //     validationLevel: 'off',
+      //     validationAction: 'warn',
+      //   },
+      // }
+      ).then((network) => { resolve(network); }).catch((err) => {
       reject(err);
     });
   });
