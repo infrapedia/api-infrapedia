7a1eb381315a093844353bad3ecca58f54be69b4
Jose Martinez <martinezjoja@gmail.com>
2020-05-12 14:59:17 -0500

    margins

--- routes/bymodules/issues.js
+++ routes/bymodules/issues.js
@@ -1,31 +1,31 @@
 module.exports = {
-  callEndPoints: (router, controllers, response ) => {
+  callEndPoints: (router, controllers, response) => {
     const statics = require('../../lib/middleware/statics');
     router.post(`${process.env._ROUTE}/auth/issues/report`, (req, res) => {
       // console.log(req.body)
       controllers.issues.addReport(req.headers.userid, req.body)
-         .then((r) => { response.success(res, r); })
-         .catch((e) => { response.err(res, e); });
+        .then((r) => { response.success(res, r); })
+        .catch((e) => { response.err(res, e); });
     });
     router.get(`${process.env._ROUTE}/auth/issues/reports`, (req, res) => {
       controllers.issues.reports(req.headers.userid, req.query.page)
-         .then((r) => { response.success(res, r, false); })
-         .catch((e) => { response.err(res, e); });
+        .then((r) => { response.success(res, r, false); })
+        .catch((e) => { response.err(res, e); });
     });
     router.get(`${process.env._ROUTE}/auth/issues/myreports`, (req, res) => {
       controllers.issues.myReports(req.headers.userid, req.query.page)
-         .then((r) => { response.success(res, r, false); })
-         .catch((e) => { response.err(res, e); });
+        .then((r) => { response.success(res, r, false); })
+        .catch((e) => { response.err(res, e); });
     });
     router.get(`${process.env._ROUTE}/auth/issues/view/:elemnt/:id`, (req, res) => {
       controllers.issues.viewReport(req.headers.userid, req.params.id, req.params.elemnt)
-         .then((r) => { response.success(res, r, false); })
-         .catch((e) => { response.err(res, e); });
+        .then((r) => { response.success(res, r, false); })
+        .catch((e) => { response.err(res, e); });
     });
     router.delete(`${process.env._ROUTE}/auth/issues/delete/:id`, (req, res) => {
       controllers.issues.deleteMyReport(req.headers.userid, req.params.id)
-         .then((r) => { response.success(res, r); })
-         .catch((e) => { response.err(res, e); });
+        .then((r) => { response.success(res, r); })
+        .catch((e) => { response.err(res, e); });
     });
   },
 };
