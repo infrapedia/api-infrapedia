8e3ef7923750685f1ef006b9049c450881e838bb
Jose Martinez <martinezjoja@gmail.com>
2020-05-11 21:27:02 -0500

    Removed, get the first element

--- controllers/class/Map.js
+++ controllers/class/Map.js
@@ -136,7 +136,7 @@ class Map {
             },
           ]).toArray((err, r) => {
             if (err) reject({ m: err });
-            resolve({ m: 'Loaded', r: r[0] });
+            resolve({ m: 'Loaded', r: r });
           });
         }).catch((e) => {});
       } catch (e) {
