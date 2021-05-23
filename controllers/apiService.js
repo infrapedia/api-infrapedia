d0eda38bc6d950975eb8a6b8da407024fd197a48
Jose Martinez <martinezjoja@gmail.com>
2021-05-23 07:32:06 -0500

    |7.1.20210523|- Api service testing removed |

--- controllers/apiService.js
+++ controllers/apiService.js
@@ -1,10 +1,10 @@
-const redisClient = require('../config/redis');
-
-let apiService = require('./class/apiService');
-
-// eslint-disable-next-line new-cap
-apiService = new apiService();
-
-module.exports = {
-  checkKey: (usr, domain) => apiService.checkKey(usr, domain),
-};
+// const redisClient = require('../config/redis');
+//
+// let apiService = require('./class/apiService');
+//
+// // eslint-disable-next-line new-cap
+// apiService = new apiService();
+//
+// module.exports = {
+//   checkKey: (usr, domain) => apiService.checkKey(usr, domain),
+// };
