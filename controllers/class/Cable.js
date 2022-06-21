30eb230ecee2abdb9da0beb9f7698691249d8027
José Javier <7897954+heyjoja@users.noreply.github.com>
2022-06-21 13:11:11 -0500

    Update Cable.js

--- controllers/class/Cable.js
+++ controllers/class/Cable.js
@@ -865,13 +865,12 @@ class Cable {
                   let a = (Array.isArray(r[0])) ? r[0][0] : r[0];
                   let b = (Array.isArray(r[r.length - 1])) ? r[r.length - 1][0] : [r.length - 1];
                   if( id == '61e8b51612b8ed7497b00a75'){
-                    console.log( '-------- Validating-------', id, [a,b])
+                    console.log( '-------- Validating-------', id, (Array.isArray(r[0)) ? [r[0], r[r.length - 1]] : r )
                   } else{
-                    console.log( '-------- no -------', id, [a,b])
+                    console.log( '-------- no -------', id, (Array.isArray(r[0)) ? [r[0], r[r.length - 1]] : r )
                   }
                   resolve({ m: 'Loaded', 
-                           r: [a, b]
-                           // [r[0], r[r.length - 1]] 
+                           r: (Array.isArray(r[0)) ? [r[0], r[r.length - 1]] : r 
                           });
                 }).catch((e) => { reject({ m: e }); });
               }
