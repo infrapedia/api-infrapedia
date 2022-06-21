7c494db52d109a4c6c76a1cc3d6cda5decb77ccf
José Javier <7897954+heyjoja@users.noreply.github.com>
2022-06-21 14:01:54 -0500

    Update Cable.js

--- controllers/class/Cable.js
+++ controllers/class/Cable.js
@@ -14,7 +14,7 @@ class Cable {
   constructor() {
     this.model = require('../../models/cable.model');
   }
-
+17901
   add(user, data) {
     return new Promise((resolve, reject) => {
       try {
